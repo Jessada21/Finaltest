9f11cc61cd560db38f59fefbb96a07faee72d489
JidapaBubu <jidapa4499@gmail.com>
2023-05-15 16:16:33 +0700

    Test1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Finaltest
+{
+class Program {
+    static void Main(string[] args) {
+        int city = int.Parse(Console.ReadLine());
+        int[] num = new int[city];
+                string namecity;
+         namecity = Console.ReadLine();
+        int collaboration = int.Parse(Console.ReadLine());
+        }
+        }
+        }
+        
+
